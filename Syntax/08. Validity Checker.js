0793525844f7288787e7f74aff9c76239bb0beb9
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-01-16 12:00:54 +0200

    Update 08. Validity Checker.js

--- Syntax/08. Validity Checker.js
+++ Syntax/08. Validity Checker.js
@@ -5,7 +5,7 @@ if(Number.isInteger(distance)){
     console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
     console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
 }else{
-    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is unvalid`);
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
 }
 }
 ValidityChecker(3, 0, 0, 4)
